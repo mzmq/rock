20fcabcc272190aa7626766c39f765b65f78209d
Mohammad AL-Qannas <44414204+mzmq@users.noreply.github.com>
2022-03-31 19:25:33 +0300

    add new page and add class
    Signed-off-by: Mohammad Alqannas <mohammadziadalqannas@gmail.com>
    Signed-off-by: Mohammad AL-Qannas <44414204+mzmq@users.noreply.github.com>

--- lib/models/details.dart
+++ lib/models/details.dart
@@ -1,8 +1,22 @@
+import 'package:flutter/foundation.dart';
+
 class Details {
-  String id; 
- String title ; 
-  Stirng subTitle ; 
-  
-  Details({required this.id , required this.title, required this.sybTitle , });
-  
+  String id;
+  String title;
+
+  String subTitle;
+
+  Details({
+    required this.id,
+    required this.title,
+    required this.subTitle,
+  });
+}
+class DetailsProvider with ChangeNotifier{
+
+  final List<Details> _item =[] ;
+  List<Details> get item {
+    return [..._item];
+  }
+
 }
